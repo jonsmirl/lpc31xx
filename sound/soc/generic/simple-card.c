6ec7c0cfb01c3cdad386094ddb70251b54462a7c
Jon Smirl <jonsmirl@gmail.com>
2014-08-15 21:30:32 -0400

    Refresh of pwm

--- sound/soc/generic/simple-card.c
+++ sound/soc/generic/simple-card.c
@@ -8,9 +8,6 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-
-#define DEBUG
-
 #include <linux/clk.h>
 #include <linux/device.h>
 #include <linux/module.h>
